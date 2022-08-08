0f733129e81fe54ffc58eaf12cc88e345195c4b7
Via Chloé <104568796+NyandereCat@users.noreply.github.com>
2022-08-08 14:53:31 +0200

    Add files via upload

--- front/js/index.js
+++ front/js/index.js
@@ -1,8 +1,48 @@
-let sofaData = [];
+main();
+async function main() {
+  const articles = await getArticles(); /* Attendre que la promesse est résolu. Peut être utilisé seulement dans les fonctions en syncrone */
+  console.log(articles);
+  displayArticles(articles);
+}
 
-const fetchSofa = async ()=> {
-    await fetch("http://localhost:3000/api/products") /*await pour attendre qu'il aille chercher la requête */
-    .then((res) => console.log (res.json()),
-    );
-};
-fetchSofa();
+function getArticles() {
+  return fetch("http://localhost:3000/api/products") /* fetch = allez chercher */
+    .then(function (responseHttp) {
+      return responseHttp.json();
+    })
+    .then(function (articles) {
+      return articles;
+    })
+    .catch(function (error) { /* Si APi est down on ajoute catch. Si Fetch n'arrive pas à destination */
+      alert(error);
+    })
+}
+
+function displayArticles(articles) {
+  let items = document.getElementById("items");
+  articles.forEach((article) =>{
+
+
+    let balise_a = document.createElement("a");
+    let balise_article = document.createElement("article");
+    let balise_img = document.createElement("img");
+    let balise_h3 = document.createElement("h3");
+    let balise_p = document.createElement("p");
+
+    balise_h3.innerHTML = article.name;
+    balise_p.innerHTML = article.description;
+    balise_img.setAttribute("src",article.imageUrl);
+    balise_img.setAttribute("alt",article.altTxt);
+    balise_h3.setAttribute("class","productName");
+    balise_h3.setAttribute("class","productDescription");
+    balise_a.setAttribute("href","product.html?id="+ article._id);
+
+
+    balise_article.appendChild(balise_img);
+    balise_article.appendChild(balise_h3);
+    balise_article.appendChild(balise_p);
+    balise_a.appendChild(balise_article);
+    items.appendChild(balise_a);
+  });
+}
+
---
+++ front/js/product.js
@@ -0,0 +1,59 @@
+const str = window.location; // permet de récupérer l'url de la page
+const url = new URL(str);
+const id = url.searchParams.get("id");
+const host ="https://nyanderecat.github.io/Kanap/";
+const objectURL = host + "api/products/" + id;
+
+let cardsFetch = function () {
+    fetch(objectURL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // get data image
+        let img = document.querySelector(".item__img");
+        img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+        // data.name and title
+        let name = document.getElementById("title");
+        name.innerHTML = data.name;
+        let title = document.querySelector("title");
+        title.innerHTML = data.name;
+        // price
+        let price = document.getElementById("price");
+        price.innerHTML = `${data.price}`;
+        // description
+        let description = document.getElementById("description");
+        description.innerHTML = data.description;
+        // colors
+        let color = document.getElementById("colors");
+        for (i = 0; i < data.colors.length; i++) {
+          color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
+        }
+      });
+  };
+
+
+  cardsFetch();
+  function qtyValue() {
+    let qty = document.getElementById("quantity");
+    return qty.value;
+  }
+
+  function colorValue() {
+    let color = document.getElementById("colors");
+    return color.value;
+  }
+
+  // HTML element : button add to cart
+const toCartBtn = document.getElementById("addToCart");
+const goToCartButton = document.getElementById("goToCart");
+goToCartButton.style.display = "none";
+// at button press : toCartBtn, function addCart that activates the 2 other function by click
+toCartBtn.addEventListener("click", () => {
+  let qty = parseInt(qtyValue());
+  let color = colorValue();
+  add2Cart(id, color, qty);
+  goToCartButton.style.display = "block";
+});
+goToCartButton.addEventListener("click", () => {
+  window.location.href = "./cart.html";
+});
